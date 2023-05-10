eaf2251345a1e2cdb9571c6e26d965c4102aac6f
Kevin Lu <kevinlu1248@gmail.com>
2023-05-09 17:26:50 -0700

    mvp

---
+++ scripts/generator.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from fastT5 import get_onnx_model
+from transformers import AutoTokenizer
+
+
+@dataclass
+class CommitMessageGenerator:
+    model_name = "SEBIS/code_trans_t5_base_commit_generation_transfer_learning_finetune"
+    models_folder = "models/ct-base-commits-fastt5-quantized"
+
+    def __post_init__(self):
+        self.model = get_onnx_model(self.model_name, self.models_folder)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, max_length=512)
+
+    def generate(self, diff):
+        token = self.tokenizer(
+            diff, 
+            return_tensors='pt', 
+            truncation=True,
+            max_length=512
+        )
+        tokens = self.model.generate(
+            input_ids=token['input_ids'],
+            attention_mask=token['attention_mask'],
+            num_beams=2,
+            max_new_tokens=20,
+        )
+        return self.tokenizer.decode(
+            tokens.squeeze(), 
+            skip_special_tokens=True
+        )
+    
+    def post_process(self, message: str) -> str:
+        message = message.capitalize()
+        message, *_ = message.split(".")
+        return message
+    
+    def __call__(self, diff: str) -> str:
+        return self.post_process(self.generate(diff))
---
+++ scripts/test_commit.py
@@ -0,0 +1,6 @@
+from scripts.generator import CommitMessageGenerator
+
+if __name__ == "__main__":
+    diff = open("models/example.diff").read()
+    generator = CommitMessageGenerator()
+    print(generator(diff))
